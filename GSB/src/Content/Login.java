571f94e8db2a49c02c3c571fe2c60084953ce0d7
Lauriane Godonou <godonoulauriane@yahoo.fr>
2022-04-15 16:16:32 +0200

    modif laudd

---
+++ GSB/src/Content/Login.java
@@ -0,0 +1,12 @@
+package Content;
+
+import javax.swing.*;
+
+public class Login {
+    private JButton OKButton;
+    private JLabel inditent;
+    private JTextField case_idente;
+    private JPasswordField case_mdp;
+    private JLabel mdp;
+    private JButton quitter_button;
+}
